97e4c092c4e6c2f403b427963eac614f674e02b6
Aqua <MangoMade@users.noreply.github.com>
2017-04-13 10:22:33 +0800

    project

---
+++ VerificationCodeButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VerificationCodeButton
+//
+//  Created by Aqua on 2017/4/13.
+//  Copyright © 2017年 Aqua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
